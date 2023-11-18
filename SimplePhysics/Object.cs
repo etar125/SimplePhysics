a68c8f639ead4d38b3a567e662763d385551971d
etar125 <116297277+etar125@users.noreply.github.com>
2023-11-18 11:15:39 +0300

    0.01

---
+++ SimplePhysics/Object.cs
@@ -0,0 +1,72 @@
+﻿// etar125
+using System;
+using System.Drawing;
+using System.ComponentModel;
+using SimplePhysics;
+using System.Threading;
+
+namespace SimplePhysics
+{
+	public class Object
+	{
+		public Point Location;
+		public Size Size;
+		public int Weight;
+		public Physics Main;
+		BackgroundWorker bw = new BackgroundWorker();
+		
+		public Object() { }
+		
+		public Object(Point loc, Size siz, int wei, Physics mai)
+		{
+			Location = loc;
+			Size = siz;
+			Weight = wei;
+			Main = mai;
+		}
+		
+		public void Gravity()
+		{
+			Location.Y++;
+			if(InterectsWithAny())
+				Location.Y--;
+		}
+		
+		public void Start()
+		{
+			bw.DoWork += (object sender, DoWorkEventArgs e) =>
+			{
+				while(true)
+				{
+					Gravity();
+					Thread.Sleep(10000 / Weight);
+				}
+			};
+			bw.RunWorkerAsync();
+		}
+		
+		public void Stop() {
+			bw.CancelAsync();
+		}
+		
+		public bool InterectsWith(Object obj) {
+			return new Rectangle(Location, Size).IntersectsWith(new Rectangle(obj.Location, obj.Size));
+		}
+		
+		public bool InterectsWithAny() {
+			foreach(Object obj in Main.Scene) {
+				if(new Rectangle(Location, Size).IntersectsWith(new Rectangle(obj.Location, obj.Size)))
+					return true;
+			}
+			return false;
+		}
+		
+		public Object InterectsWithWhat() {
+			foreach(Object obj in Main.Scene) {
+				if(new Rectangle(Location, Size).IntersectsWith(new Rectangle(obj.Location, obj.Size)))
+					return obj;
+			}
+			return null;
+		}
+	}
+}
---
+++ SimplePhysics/Physics.cs
@@ -0,0 +1,40 @@
+﻿// etar125
+using System;
+using System.Collections.Generic;
+using SimplePhysics;
+
+namespace SimplePhysics
+{
+	public class Physics
+	{
+		public List<Object> Scene = new List<Object> { };
+		
+		public Physics() { }
+		
+		public void AddObject(Object obj) {
+			Scene.Add(obj);
+		}
+		
+		public void RemoveObject(Object obj) {
+			Scene.Remove(obj);
+		}
+		
+		public void Clear() {
+			Scene.Clear();
+		}
+		
+		public void RemoveLast() {
+			Scene.RemoveAt(Scene.Count - 1);
+		}
+		
+		public void StartAll() {
+			foreach(Object obj in Scene)
+				obj.Start();
+		}
+		
+		public void StopAll() {
+			foreach(Object obj in Scene)
+				obj.Stop();
+		}
+	}
+}
